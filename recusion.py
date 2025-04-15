a1a276c1360885f8d7d4dd61a67327e60ac01faa
Sid.py <153026134+BscCanCode@users.noreply.github.com>
2025-04-15 21:16:27 +0530

    updated_factorial_recursion
    n this updated version, I have resolved some known issues and added a few basic extra features.

--- recusion.py
+++ recusion.py
@@ -4,13 +4,28 @@ def recursion(n):
         return 1
     else:
         return n* recursion(n-1)
-
-try:
-    n=int(input("Enter the number to factorial of:"))
-    if n<0:
-        print("Negative numbers don't have factorial!")
-    else:
-        print(f"The factorial of {n} is:",recursion(n))
-except ValueError:
-    print("Only Positive integers have factorial,not strings or negative number")
-    sys.exit()
+while True:
+    try:
+        n=int(input("Enter the number between (1-100) to find factorial of:"))
+        if n<0:
+            print("Negative numbers don't have a factorial")
+        elif n<=100:
+            print(f"The factorial of {n} is:",recursion(n))
+            print("-------------------------------------------------------------")
+        else:
+            print("To avoid stack overflow you can enter value between 1-100")
+            break
+        b=input("Would you like to continue? yes/no:")
+        if b=="no":
+            print("Exit is done")
+            break
+        elif b=="yes":
+            continue
+        else:
+            print("You should enter either a yes or no")
+            break
+            
+        
+    except ValueError:
+        print("Only positive integers factorials are accepted,Strings does not have a concept named factorial")
+        break
